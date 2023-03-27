a94d33d57db272bf5176dbeae0c8c0239450a00e
KAD105 <kazem_abdi@yahoo.com>
2023-03-27 18:26:14 +0200

    add all

--- src/components/Layout/MainTable.js
+++ src/components/Layout/MainTable.js
@@ -12,11 +12,21 @@ const MainTable = (props) => {
         <h1>Resource Booking</h1>
       </header>
       <div className={classes["booking-table-btns"]}>
-        <CartButton className={classes["booking-table-btns--modified"]} />
-        <CartButton className={classes["booking-table-btns--modified"]} />
-        <CartButton className={classes["booking-table-btns--modified"]} />
-        <CartButton className={classes["booking-table-btns--modified"]} />
-        <CartButton className={classes["booking-table-btns--modified"]} />
+        <CartButton className={classes["booking-table-btns--modified"]}>
+          Log out
+        </CartButton>
+        <CartButton className={classes["booking-table-btns--modified"]}>
+          Reset Password
+        </CartButton>
+        <CartButton className={classes["booking-table-btns--modified"]}>
+          Search Rooms
+        </CartButton>
+        <CartButton className={classes["booking-table-btns--modified"]}>
+          My bookings
+        </CartButton>
+        <CartButton className={classes["booking-table-btns--modified"]}>
+          All Bookings
+        </CartButton>
       </div>
     </Fragment>
   );
